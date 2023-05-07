8e27786ab10c44b1e8c34036d4deb39afa4f30d8
DiiKhanh <duykhanh.030803@gmail.com>
2023-05-07 15:55:57 +0700

    update crud user

--- src/main/java/com/projectjavasneaker/backendis216/payload/response/JwtResponse.java
+++ src/main/java/com/projectjavasneaker/backendis216/payload/response/JwtResponse.java
@@ -10,12 +10,31 @@ public class JwtResponse {
     private String email;
     private List<String> roles;
 
+    private String address;
+    private String phone;
+    private String gender;
+
+    private String birth;
+
     public JwtResponse(String accessToken, Long id, String username, String email, List<String> roles) {
         this.token = accessToken;
         this.id = id;
         this.username = username;
         this.email = email;
         this.roles = roles;
+    }
+    public JwtResponse(String accessToken, Long id, String username, String email, List<String> roles
+    ,String address, String phone, String gender, String birth
+                       ) {
+        this.token = accessToken;
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.roles = roles;
+        this.address = address;
+        this.phone = phone;
+        this.gender = gender;
+        this.birth = birth;
     }
 
     public String getAccessToken() {
@@ -60,5 +79,37 @@ public class JwtResponse {
 
     public List<String> getRoles() {
         return roles;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getBirth() {
+        return birth;
+    }
+
+    public void setBirth(String birth) {
+        this.birth = birth;
     }
 }
